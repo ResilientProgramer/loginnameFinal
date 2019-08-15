d34c443199527bc78f352ed02326eca4c36e4665
ResilientProgramer <52796008+ResilientProgramer@users.noreply.github.com>
2019-08-15 11:24:46 -0400

    Add files via upload

---
+++ assets/Data/chairData.ts
@@ -0,0 +1,6 @@
+import {Chair } from '../../app/chair';
+
+export const ChairData : Chair[] = [
+    {brand: 'Ikea', make: 'wood',country:'China'},
+    {brand: 'Ikea', make: 'plastic',country:'Korea'}
+]
